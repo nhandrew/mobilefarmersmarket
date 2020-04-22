ba59fb1823d04d398159ea2c761333b4fc9b257b
Andrew Julow <andrewjulow@Andrews-iMac.local>
2020-04-22 15:10:34 -0400

    End of Episode 3

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,13 +1,29 @@
 import 'package:farmers_market/src/routes.dart';
 import 'package:farmers_market/src/screens/login.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
 
-class App extends StatelessWidget{
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Login(),
-      onGenerateRoute: Routes.materialRoutes,
-    );
+    return PlatformApp();
+  }
+}
+
+class PlatformApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    if (Platform.isIOS) {
+      return CupertinoApp(
+        home: Login(),
+        onGenerateRoute: Routes.cupertinoRoutes,
+      );
+    } else {
+      return MaterialApp(
+        home: Login(),
+        onGenerateRoute: Routes.materialRoutes,
+      );
+    }
   }
 }
