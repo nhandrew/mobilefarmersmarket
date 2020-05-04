4f87762ed3e8c775eb9e4a0db3807637c2b40df9
Andrew Julow <andrewjulow@Andrews-iMac.local>
2020-05-04 14:04:28 -0400

    End Part 14

---
+++ lib/src/blocs/auth_bloc.dart
@@ -0,0 +1,44 @@
+
+import 'dart:async';
+
+import 'package:rxdart/rxdart.dart';
+import 'package:rxdart/subjects.dart';
+
+final RegExp regExpEmail = RegExp(
+    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+
+class AuthBloc {
+  final _email = BehaviorSubject<String>();
+  final _password = BehaviorSubject<String>();
+
+  //Get Data
+  Stream<String> get email => _email.stream.transform(validateEmail);
+  Stream<String> get password => _password.stream;
+  Stream<bool> get isValid => CombineLatestStream.combine2(email, password, (email,password)=> true);
+
+  //Set Data
+  Function(String) get changeEmail => _email.sink.add;
+  Function(String) get changePassword => _password.sink.add;
+
+  dispose(){
+    _email.close();
+    _password.close();
+  }
+
+  //Transformers
+  final validateEmail = StreamTransformer<String,String>.fromHandlers(handleData: (email, sink){
+    if (regExpEmail.hasMatch(email.trim())){
+      sink.add(email.trim());
+    }else {
+      sink.addError('Must Be Valid Email Address');
+    }
+  });
+
+    final validatePassword = StreamTransformer<String,String>.fromHandlers(handleData: (password, sink){
+    if (password.length >= 8){
+      sink.add(password.trim());
+    }else {
+      sink.addError('8 Character Minimum');
+    }
+  });
+}
