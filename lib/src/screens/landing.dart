f1dc34b9cbbc973b9a97f7dcc4f163b07067ca8e
Andrew Julow <andrewjulow@Andrews-iMac.local>
2020-07-27 14:42:28 -0400

    End of Part 40

--- lib/src/screens/landing.dart
+++ lib/src/screens/landing.dart
@@ -1,28 +1,107 @@
+import 'package:farmers_market/src/styles/base.dart';
+import 'package:farmers_market/src/styles/colors.dart';
+import 'package:farmers_market/src/styles/text.dart';
 import 'package:farmers_market/src/widgets/button.dart';
+import 'package:farmers_market/src/widgets/list_tile.dart';
+import 'package:farmers_market/src/widgets/sliver_scaffold.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 
-class Landing extends StatelessWidget{
+class Landing extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    if (Platform.isIOS){
-      return CupertinoPageScaffold(  
-        child: pageBody(context),
+    if (Platform.isIOS) {
+      return AppSliverScaffold.cupertinoSliverScaffold(
+        navTitle: 'Upcoming Markets',
+        pageBody: Scaffold(body: pageBody(context)),
       );
     } else {
-      return Scaffold(body:pageBody(context));
+      return AppSliverScaffold.materialSliverScaffold(
+          navTitle: 'Upcoming Markets', pageBody: pageBody(context));
     }
   }
 
-  Widget pageBody(BuildContext context){
-    return Column(  
+  Widget pageBody(BuildContext context) {
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        AppButton(  
-          buttonText: 'Vendor Page',
-          buttonType: ButtonType.Straw,
-          onPressed: () => Navigator.pushNamed(context, '/vendor'),
+        Flexible(
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                child: Image.asset('assets/images/market.jpg'),
+                top: -10.0,),
+              Positioned(
+                bottom: 10.0,
+                right: 10.0,
+                child: GestureDetector(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: AppColors.lightblue,
+                        borderRadius:
+                            BorderRadius.circular(BaseStyles.borderRadius)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('Vendor Page',
+                          style: TextStyles.buttonTextLight),
+                    ),
+                  ),
+                  onTap: () => Navigator.of(context).pushNamed('/vendor'),
+                ),
+              )
+            ],
+          ),
+          flex: 2,
+        ),
+        Flexible(
+          child: ListView(
+            children: <Widget>[
+              AppListTile(
+                date: '4',
+                month: 'Oct',
+                title: 'Anytown Farmer\'s Market',
+                location: '1 Main St, Anytown, US',
+                acceptingOrders: true,
+              ),
+              AppListTile(
+                  date: '14',
+                  month: 'Oct',
+                  title: 'Anytown Farmer\'s Market',
+                  location: '1 Main St, Anytown, US'),
+              AppListTile(
+                  date: '20',
+                  month: 'Oct',
+                  title: 'Anytown Farmer\'s Market',
+                  location: '1 Main St, Anytown, US'),
+              AppListTile(
+                  date: '24',
+                  month: 'Oct',
+                  title: 'Anytown Farmer\'s Market',
+                  location: '1 Main St, Anytown, US'),
+              AppListTile(
+                  date: '24',
+                  month: 'Oct',
+                  title: 'Anytown Farmer\'s Market',
+                  location: '1 Main St, Anytown, US'),
+              AppListTile(
+                  date: '24',
+                  month: 'Oct',
+                  title: 'Anytown Farmer\'s Market',
+                  location: '1 Main St, Anytown, US'),
+              AppListTile(
+                  date: '24',
+                  month: 'Oct',
+                  title: 'Anytown Farmer\'s Market',
+                  location: '1 Main St, Anytown, US'),
+              AppListTile(
+                  date: '24',
+                  month: 'Oct',
+                  title: 'Anytown Farmer\'s Market',
+                  location: '1 Main St, Anytown, US'),
+            ],
+          ),
+          flex: 3,
         )
       ],
     );
---
+++ lib/src/widgets/list_tile.dart
@@ -0,0 +1,49 @@
+import 'package:farmers_market/src/styles/base.dart';
+import 'package:farmers_market/src/styles/colors.dart';
+import 'package:farmers_market/src/styles/text.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class AppListTile extends StatelessWidget {
+  final String month;
+  final String date;
+  final String title;
+  final String location;
+  final bool acceptingOrders;
+
+  AppListTile({
+    @required this.month,
+    @required this.date,
+    @required this.title,
+    @required this.location,
+    this.acceptingOrders = false
+  });
+
+  @override
+  Widget build(BuildContext context) {
+      return Column(
+        children: <Widget>[
+          ListTile( 
+            leading: CircleAvatar(  
+              radius: 25.0,
+              backgroundColor: AppColors.lightblue,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(month,style: TextStyle(color:Colors.white, fontSize: 12.0),),
+                  Text(date, style: TextStyles.buttonTextLight)
+                ],),
+            ),
+            title: Text(title,style:TextStyles.subtitle),
+            subtitle: Text(location),
+            trailing:(acceptingOrders) ? Icon(FontAwesomeIcons.shoppingBasket,color: AppColors.darkblue,): Text(''),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: BaseStyles.listFieldHorizontal),
+            child: Divider(color:AppColors.lightgray),
+          )
+        ],
+      );
+
+  }
+}
